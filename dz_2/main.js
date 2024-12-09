552ea31683a5157232255e4fb8ca1e1f58d1180d
Sanjar0006 <drakengo299@gmail.com>
2024-12-09 03:29:49 -0600

    Add files via upload

---
+++ dz_2/main.js
@@ -0,0 +1,29 @@
+    var colors = prompt ("Выберите цвет")
+
+    if (colors === "red") {
+        alert("Stop")
+    }
+
+   else if (colors === "yellow") {
+        alert("Wait")
+    }
+    
+   else if (colors === "green") {
+        alert("goo");
+    } else {
+        alert("Надо выводить цвета")
+    }
+
+   for (let i = 20; i >= 0; i--) {
+       console.log(i)
+   }
+    console.log(colors);
+
+    var arr = [10,20,30,50,235,3000];
+    for (var i of arr) {
+        var string = String(i)
+        if (string[0] == 1|| string[0]== 2 || string[0]== 5){
+            console.log(i)
+        }
+    }
+
